9533569234bddf24d35184d112b32769c44859a3
manuelagarcia36 <manuelagarcialira@gmail.com>
2023-04-14 22:31:10 -0300

    fix button aspects

--- practico-03/ViewController.swift
+++ practico-03/ViewController.swift
@@ -14,25 +14,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UITextField!
     @IBOutlet weak var followersNumberLabel: UITextField!
     @IBOutlet weak var followingNumberLabel: UITextField!
-    @IBOutlet weak var FollowingButton: UIButton!
+    @IBOutlet weak var followingButton: UIButton!
     @IBOutlet weak var moreOptionsButton: UIButton!
     @IBOutlet weak var informationStack: UIStackView!
     @IBOutlet weak var stackResultView: UIView!
+    
     //Colores
-    private let primaryColor = UIColor(red: 56/255, green: 117/255, blue: 233/255, alpha: 1)
+    //private let primaryColor = UIColor(red: 56/255, green: 117/255, blue: 233/255, alpha: 1)
+    private let facebookBlue = UIColor(red: 59/255, green: 89/255, blue: 152/255, alpha: 1.0)
+    let facebookLightBlue = UIColor(red: 179/255, green: 203/255, blue: 252/255, alpha: 1.0)
+    
+    private let facebookDarkGray = UIColor(red: 35/255, green: 35/255, blue: 35/255, alpha: 1.0)
+    private let facebookLightGray = UIColor(red: 216/255, green: 216/255, blue: 216/255, alpha: 1.0)
+    private let facebookWhite = UIColor.white
+    private let facebookBlack = UIColor.black
+
     private let secondaryColor = UIColor(red: 173/255, green: 202/255, blue: 250/255, alpha:1 )
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         // Do any additional setup after loading the view.
         perfilImage.layer.cornerRadius = perfilImage.bounds.height / 2
         perfilImage.clipsToBounds = true
+        perfilImage.layer.borderWidth = 2
+        perfilImage.layer.borderColor = UIColor.white.cgColor
+        
         nameLabel.text = "Luis Suarez"
         followersNumberLabel.text = "18M"
         followingNumberLabel.text = "23"
         
+        followingButton.backgroundColor = facebookBlue
+        followingButton.tintColor = facebookWhite
+        followingButton.setTitle("Follow", for: .normal)
+        let followImage = UIImage(systemName: "person.badge.plus")
+        followingButton.setImage(followImage, for: .normal)
+        followingButton.layer.cornerRadius = followingButton.bounds.height/4
+        
+        moreOptionsButton.layer.cornerRadius = moreOptionsButton.bounds.width/4
+        moreOptionsButton.backgroundColor = facebookLightGray
+        moreOptionsButton.tintColor = facebookBlack
         
         let listTitleButtons = ["Post", "Info", "Friends", "More"]
         
@@ -41,25 +62,15 @@ class ViewController: UIViewController {
         informationStack.distribution = .fillEqually
         informationStack.spacing = 10
         
-        /** for title in listTitleButtons {
-         let button = UIButton()
-         button.setTitle(title, for: .normal)
-         button.setTitleColor(UIColor.black, for: .normal)
-         
-         informationStack.addArrangedSubview(button)
-         } */
-        
-        
         for title in listTitleButtons {
             let button = UIButton()
             button.setTitle(title, for: .normal)
-            button.setTitleColor(.black, for: .normal)
-            button.backgroundColor = .white
+            button.setTitleColor(facebookBlack, for: .normal)
+            button.backgroundColor = facebookWhite
             button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
             
             informationStack.addArrangedSubview(button)
         }
-        
         
         view.addSubview(informationStack)
         
@@ -68,18 +79,19 @@ class ViewController: UIViewController {
     
     @objc func buttonTapped(_ sender: UIButton) {
         // Deseleccionar los otros botones
-        for case let button as UIButton in informationStack.arrangedSubviews where button != sender {
+        for case let button as UIButton in informationStack.arrangedSubviews where
+        button != sender {
             button.isSelected = false
-            button.backgroundColor = .white
+            button.backgroundColor = facebookWhite
+            button.setTitleColor(facebookBlack, for: .normal)
         }
         
         // Seleccionar el botón actual
         sender.isSelected = true
-        sender.backgroundColor = secondaryColor
-        sender.setTitleColor(primaryColor, for: .normal)
+        sender.backgroundColor = facebookLightBlue
+        sender.setTitleColor(facebookWhite, for: .normal)
         sender.layer.cornerRadius = sender.bounds.height/2
         sender.clipsToBounds = true
-        
         
         switch sender.currentTitle {
            case "Post":
@@ -100,11 +112,6 @@ class ViewController: UIViewController {
         default:
                break
            }
-        
-        
-        // Aquí puedes escribir el código para manejar el evento del botón
-        print("Button tapped: \(sender.currentTitle ?? "")")
     }
-    
 }
 
